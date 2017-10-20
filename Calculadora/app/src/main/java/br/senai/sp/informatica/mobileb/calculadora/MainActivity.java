a268b23313b0c61837c70177ee45c2cb1b1697aa
WEB <viviteodoro19@gmail.com>
2017-10-20 21:39:47 -0200

    ok

--- Calculadora/app/src/main/java/br/senai/sp/informatica/mobileb/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/br/senai/sp/informatica/mobileb/calculadora/MainActivity.java
@@ -2,12 +2,49 @@ package br.senai.sp.informatica.mobileb.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+    private EditText campo;
+    private Button botao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        campo = (EditText) findViewById(R.id.edResultado);
     }
+
+    public void clear(View view){
+        campo.setText(null);
+        if(!botao.findViewById(R.id.btnVirgula).isEnabled()) {
+            botao.findViewById(R.id.btnVirgula).setEnabled(false);
+        }
+    }
+
+    public void numeros(View view){
+        botao = (Button)view;
+        String num = botao.getText().toString();
+
+        if(num.equals(",") && campo.getText().toString().isEmpty()){
+            campo.setText(campo.getText() + "0" + num);
+            botao.findViewById(R.id.btnVirgula).setEnabled(false);
+        }
+        else if(num.equals(",")){
+            campo.setText(campo.getText() + num);
+            botao.findViewById(R.id.btnVirgula).setEnabled(false);
+        }
+        else if(campo.getText().toString().startsWith("0") && !campo.getText().toString().startsWith("0,")){
+            campo.setText(num);
+        }
+        else {
+            campo.setText(campo.getText() + num);
+        }
+    }
+
+    public void operacoes(View view){}
+
 }
