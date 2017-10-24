fa5048175b16ba772964e9dc2e8de5c1597c6a9b
VIVIANE TEODORO DA SILVA <45948375811@jpmorgan.sp>
2017-10-24 18:41:38 -0200

    ok

--- Calculadora/app/src/main/java/br/senai/sp/informatica/mobileb/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/br/senai/sp/informatica/mobileb/calculadora/MainActivity.java
@@ -12,34 +12,26 @@ public class MainActivity extends AppCompatActivity{
     private EditText campo;
     private Button botao;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        campo = (EditText) findViewById(R.id.edResultado);
-    }
-
-    public void clear(View view){
-        campo.setText(null);
-    }
-
-    public void numeros(View view){
-        botao = (Button)view;
-        String num = botao.getText().toString();
-
-        if(num.equals(",") && campo.getText().toString().isEmpty()){
-            campo.setText(campo.getText() + "0" + num);
-        }
-        else if(num.equals(",")){
-            campo.setText(campo.getText() + num);
-        }
-        else if(campo.getText().toString().startsWith("0") && !campo.getText().toString().startsWith("0,")){
-            campo.setText(num);
-        }
-        else {
-            campo.setText(campo.getText() + num);
-        }
-    }
-
-    public void operacoes(View view){}
-
+   protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+       campo = (EditText) findViewById(R.id.edResultado);
+   }
+   public void clear(View view){ campo.setText(null); }
+   public void numeros(View view){
+       botao = (Button)view;
+       String num = botao.getText().toString();
+       if(num.equals(",") && campo.getText().toString().isEmpty()){
+           campo.setText(campo.getText() + "0" + num);
+       }
+       else if(campo.getText().toString().contains(",") && num.equals(",")){
+           num = "";
+           campo.setText(campo.getText() + num);
+       }
+       else if(campo.getText().toString().startsWith("0") && !campo.getText().toString().startsWith("0,") && !num.equals(",")){
+           campo.setText(num);
+       }
+       else { campo.setText(campo.getText() + num); }
+   }
+   public void operacoes(View view){}
 }
