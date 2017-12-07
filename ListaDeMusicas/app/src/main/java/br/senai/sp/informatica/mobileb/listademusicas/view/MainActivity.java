13af27b0f21786ec8f26e6ee94cca6fd35b59869
vteodoro <viviteodoro19@gmail.com>
2017-12-07 21:54:28 -0200

    ok

--- ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/MainActivity.java
+++ ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/MainActivity.java
@@ -1,28 +1,48 @@
 package br.senai.sp.informatica.mobileb.listademusicas.view;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import br.senai.sp.informatica.mobileb.listademusicas.R;
 
-public class MainActivity extends AppCompatActivity implements  EditarMusicas{
+public class MainActivity extends AppCompatActivity implements  EditarMusicas, AdapterView.OnItemClickListener, View.OnClickListener, NavigationView.OnNavigationItemSelectedListener{
     private ListView listView;
     private BaseAdapter itemLista;
     private Intent i;
     private final int EDITA_MUSICA = 0;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         itemLista = new MusicaAdapter(this);
         listView = (ListView) findViewById(R.id.lvLista);
         listView.setAdapter(itemLista);
         i = new Intent(getBaseContext(), EditarActivity.class);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, "Open navigation drawer", "Close navigation drawer");
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -52,5 +72,20 @@ public class MainActivity extends AppCompatActivity implements  EditarMusicas{
     public void executa(long id) {
         i.putExtra("id", id);
         startActivityForResult(i, EDITA_MUSICA);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
