5bbec3b4a4ff17418054cbd73faeb647431a9be1
vteodoro <viviteodoro19@gmail.com>
2018-01-11 18:48:08 -0200

    ok

--- RecyclerApp/app/src/main/java/br/senai/sp/informatica/mobileb/recyclerapp/MainActivity.java
+++ RecyclerApp/app/src/main/java/br/senai/sp/informatica/mobileb/recyclerapp/MainActivity.java
@@ -2,13 +2,33 @@ package br.senai.sp.informatica.mobileb.recyclerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import br.senai.sp.informatica.mobileb.recyclerapp.dao.MusicaDao;
+import br.senai.sp.informatica.mobileb.recyclerapp.model.Musica;
+import br.senai.sp.informatica.mobileb.recyclerapp.view.adapter.MusicaAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MusicaDao dao = MusicaDao.manager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        List<Musica> musicas = dao.getLista();
+
+        recyclerView = findViewById(R.id.rvMusicas);
+
+        recyclerView.setAdapter(new MusicaAdapter(musicas, this));
+
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layout);
     }
 }
--- RecyclerApp/app/src/main/java/br/senai/sp/informatica/mobileb/recyclerapp/view/holder/MusicaViewHolder.java
+++ RecyclerApp/app/src/main/java/br/senai/sp/informatica/mobileb/recyclerapp/view/holder/MusicaViewHolder.java
@@ -1,13 +1,18 @@
 package br.senai.sp.informatica.mobileb.recyclerapp.view.holder;
 
+import android.app.Activity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import br.senai.sp.informatica.mobileb.recyclerapp.R;
 import br.senai.sp.informatica.mobileb.recyclerapp.model.Musica;
 import br.senai.sp.informatica.mobileb.recyclerapp.view.adapter.MusicaAdapter;
 
-public class MusicaViewHolder extends RecyclerView.ViewHolder{
+public class MusicaViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
 
     public final TextView titulo;
     public final TextView artista;
@@ -17,9 +22,48 @@ public class MusicaViewHolder extends RecyclerView.ViewHolder{
     public MusicaViewHolder(final View view, final MusicaAdapter adapter){
         super(view);
         this.adapter = adapter;
+
+        view.setOnClickListener(this);
+        view.setOnClickListener(this);
+
         titulo = view.findViewById(R.id.tvTitulo);
         artista = view.findViewById(R.id.tvArtista);
     }
 
-    public void preencher(Musica musica){}
+    public void preencher(Musica musica){
+        musicaId = musica.getId();
+        titulo.setText(musica.getTitulo());
+        artista.setText(musica.getArtista());
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), musicaId.toString(), Toast.LENGTH_SHORT).show();
+        Log.d("Musica selecionada", musicaId.toString());
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+        popupMenu.getMenuInflater().inflate(R.menu.musica_options, popupMenu.getMenu());
+
+        final Activity context = (Activity)view.getContext();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener(){
+            public boolean onMenuItemClick(MenuItem item){
+                switch(item.getItemId()){
+
+                    case R.id.menuMusEditar:
+                        break;
+
+                    case R.id.menuMusDeletar:
+                        break;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+        return false;
+
+    }
 }
