103bce45af196572cd401574a72ff4a494ff535f
vteodoro <viviteodoro19@gmail.com>
2018-01-12 19:43:39 -0200

    ok

--- ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/fragment/PreferenciasFragment.java
+++ ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/fragment/PreferenciasFragment.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
-import br.senai.sp.informatica.mobileb.listademusicas.R;
-
 public class PreferenciasFragment extends PreferenceFragment{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferencias);
+        //addPreferencesFromResource();
     }
 }
