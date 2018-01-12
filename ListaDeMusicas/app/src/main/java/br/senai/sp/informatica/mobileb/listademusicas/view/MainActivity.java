65164a32e53fbe9ea5ffec81835b083a1bbac839
vteodoro <viviteodoro19@gmail.com>
2018-01-12 21:23:38 -0200

    ok

--- ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/MainActivity.java
+++ ListaDeMusicas/app/src/main/java/br/senai/sp/informatica/mobileb/listademusicas/view/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements  EditarMusicas, A
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nav);
 
         itemLista = new MusicaAdapter(this);
         listView = (ListView) findViewById(R.id.lvLista);
