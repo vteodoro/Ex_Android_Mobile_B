ea9e4e0014a9dd68bc3d4c2cdd333c953db741e1
vteodoro <viviteodoro19@gmail.com>
2018-02-16 21:50:32 -0200

    exemplo autenticação

---
+++ AuthApp/app/src/main/java/br/senai/sp/informatica/authapp/MainActivity.java
@@ -0,0 +1,91 @@
+package br.senai.sp.informatica.authapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText edEmail;
+    public EditText edSenha;
+    public EditText edAuth;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edEmail = findViewById(R.id.etEmail);
+        edSenha = findViewById(R.id.etSenha);
+        edAuth = findViewById(R.id.etToken);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void updateUI(FirebaseUser user){
+        if(user != null){
+            edAuth.setText(user.getUid());
+        }
+    }
+
+    public void novoLoginClick(View view){
+        String email = edEmail.getText().toString();
+        String senha = edSenha.getText().toString();
+
+        //showProgressDialog();
+
+        mAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else{
+                            Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        //hideProgressDialog();
+                    }
+                });
+    }
+
+    public void loginClick(View view){
+        String email = edEmail.getText().toString();
+        String senha = edSenha.getText().toString();
+
+        //showProgressDialog();
+
+        mAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else{
+                            Toast.makeText(MainActivity.this, "Falha na autenticação", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        //hideProgressDialog();
+                    }
+                });
+    }
+
+    public void logoutClick(View view){
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+}
